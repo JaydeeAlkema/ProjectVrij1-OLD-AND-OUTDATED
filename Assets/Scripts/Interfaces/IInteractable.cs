6f1b910eac229bef9412891c6f1929ed720e0912
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-06 20:22:01 +0200

    Add IInteractable
    Add Interactable Interface.

---
+++ Assets/Scripts/Interfaces/IInteractable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A simple Interactable Interface used for Objects and NPC with which the Player can interact.
+/// </summary>
+public interface IInteractable
+{
+	void Interact(GameObject Interactor);
+}
