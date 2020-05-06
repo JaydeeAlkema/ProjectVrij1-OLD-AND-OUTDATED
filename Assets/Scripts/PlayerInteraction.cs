03c7763f34182c881d0b07ff6d42160a1960fff9
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-06 21:01:54 +0200

    Add Variables and Properties
    - Add Reference to the Text Box Title Text Mesh.
    - Add Reference to the Text Box Context Text Mesh.

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerInteraction : MonoBehaviour
 {
+	#region Variables
+	[SerializeField] private TextMeshProUGUI textBoxTitleTextMesh = default;			// Reference to the Text Box Title Text Mesh Component.
+	[SerializeField] private TextMeshProUGUI textBoxContextTextMesh = default;          // Reference to the Text Box Context Text Mesh Component.
+	#endregion
+
+	#region Properties
+	public TextMeshProUGUI TextBoxTitleTextMesh { get => textBoxTitleTextMesh; set => textBoxTitleTextMesh = value; }
+	public TextMeshProUGUI TextBoxContextTextMesh { get => textBoxContextTextMesh; set => textBoxContextTextMesh = value; }
+	#endregion
+
 	private void OnTriggerEnter(Collider other)
 	{
 		other.GetComponent<IInteractable>()?.Interact(gameObject);
