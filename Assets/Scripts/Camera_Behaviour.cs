06a3a08a27fb0b976e0d78bdd509ccb0c9e0258b
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-06 21:45:09 +0200

    Fix Bad Camera movement

--- Assets/Scripts/Camera_Behaviour.cs
+++ Assets/Scripts/Camera_Behaviour.cs
@@ -20,21 +20,18 @@ public class Camera_Behaviour : MonoBehaviour
 		player = FindObjectOfType<Player_Behaviour>();
 	}
 
-	private void Update()
+	private void FixedUpdate() //Calculate the camera position after the actual player position is calculated to prevent hitching
 	{
 		Vector3 desiredPos = new Vector3(target.transform.position.x + offset.x + (player.MovementInput.x * frontOffset), offset.y, offset.z); //Calculates the desired position of the camera relative to the movement of the player
 		smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothing * Time.deltaTime); //Does the camera moving stuff
 
-		if (clamp) //is clamped? yes? then clamp
+		if(clamp) //is clamped? yes? then clamp
 		{
 			Vector3 pos = transform.position;
 			pos.x = Mathf.Clamp(transform.position.x, clampX.x, clampX.y);
 			transform.position = pos;
 		}
-	}
 
-	private void LateUpdate() //Calculate the camera position after the actual player position is calculated to prevent hitching
-	{
 		transform.position = smoothedPos;
 	}
 }
