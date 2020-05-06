0826a9cfae53a0cfb23f4f84641bd7eaa18d2492
Joris <j_d_derks@hotmail.com>
2020-05-06 20:31:32 +0200

    Add comments to player movement class

--- Assets/Scripts/Player_Behaviour.cs
+++ Assets/Scripts/Player_Behaviour.cs
@@ -6,15 +6,15 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Player_Behaviour : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Animator anim;
-    private bool idle = true;
-    private bool facingRight = true;
-    private bool facingUp = false;
+    private Rigidbody rb; //Rigidbody to add force (movement) to
+    private Animator anim; //Animator to add animations like walking animations/idle animations
+    private bool idle = true; //Is the player idle? (standing still doing nothing)
+    private bool facingRight = true; //Is the player facing right?
+    private bool facingForward = false; //Is the player facing 
 
-    [SerializeField] private float moveForce;
-    [SerializeField] private Vector3 movementInput;
-    [SerializeField] private Vector3 movementVector;
+    [SerializeField] private float movementSpeed; //The speed of how the player moves
+    [SerializeField] private Vector3 movementInput; //The input strings (Horizontal / Vertical)
+    [SerializeField] private Vector3 movementVector; //The vector the movement gets send toward
 
     //[Header("Directional Materials")]
     //#region Materials
@@ -27,25 +27,26 @@ public class Player_Behaviour : MonoBehaviour
 
     public Vector3 MovementInput { get => movementInput; private set => movementInput = value; }
 
-    private void Awake()
+    private void Awake() //Get components from same object
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
     }
 
-    private void Update()
+    private void Update() //Gets called every frame
     {
         Movement();
         //AnimateCharacter();
     }
 
-    private void Movement()
+    private void Movement() //Handles player movement
     {
-        MovementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        MovementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")); //Transforms input into vector movement
+
         if (movementInput != Vector3.zero)
-            movementVector = new Vector3(MovementInput.x * moveForce, 0f, MovementInput.z * moveForce) * Time.fixedDeltaTime;
+            movementVector = new Vector3(MovementInput.x * movementSpeed, 0f, MovementInput.z * movementSpeed) * Time.fixedDeltaTime; //If the playerinput is pressed move the player
         else
-            movementVector = Vector3.zero;
+            movementVector = Vector3.zero; //else don't move the player
 
     }
 
@@ -80,10 +81,10 @@ public class Player_Behaviour : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(transform.position + movementVector);
+        rb.MovePosition(transform.position + movementVector); //Add the force to the rigidbody to move the player
     }
 
-    private void Flip(string lookAt)
+    private void Flip(string lookAt) //Makes the player look in the direction it is moving
     {
         //SpriteRenderer sr = GetComponent<SpriteRenderer>();
         //      switch (lookAt)
