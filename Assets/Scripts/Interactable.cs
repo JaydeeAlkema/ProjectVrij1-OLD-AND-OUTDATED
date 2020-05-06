db792c02d21063c2f52f5e41755c9665c2eb6c9e
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-06 22:34:05 +0200

    Add Toggle Prompt Functionality

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Interactable : MonoBehaviour, IInteractable
 {
 	#region Variables
+	[SerializeField] private GameObject interactionPromptObject = default;      // Reference to the interaction prompt object.
+	[Space]
 	[SerializeField] private string title = "";         // Title of the Object / NPC (a.k.a. A Name)
 	[SerializeField] private string context = "";       // The context for this Object / NPC (a.k.a. A Description)
 	#endregion
@@ -14,11 +16,23 @@ public class Interactable : MonoBehaviour, IInteractable
 	public string Context { get => context; set => context = value; }
 	#endregion
 
+	#region Monobehaviour Callbacks
+	private void Start()
+	{
+		TogglePrompt(false);
+	}
+	#endregion
+
 	public void Interact(GameObject Interactor)
 	{
 		Interactor.GetComponent<PlayerInteraction>().TextBoxTitleTextMesh.text = title;
 		Interactor.GetComponent<PlayerInteraction>().TextBoxContextTextMesh.text = context;
 
 		Debug.Log(Interactor.name + " Interacted with " + gameObject.name);
+	}
+
+	public void TogglePrompt(bool toggle)
+	{
+		interactionPromptObject.SetActive(toggle);
 	}
 }
