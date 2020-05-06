b77887cbe232b28f57acce5fb4f6a30a3a040ea8
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-06 22:33:51 +0200

    Add new Functionality
    The player interaction now stores the most recent IInteractable it got in contact with. The player first gets a interaction prompt and the text box appears after the interaction key has been pressed.

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,22 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
 	#region Variables
-	[SerializeField] private TextMeshProUGUI textBoxTitleTextMesh = default;			// Reference to the Text Box Title Text Mesh Component.
+	[SerializeField] private GameObject textBoxObject = default;                        // Reference to the entire Text Box Gameobject.
+	[SerializeField] private TextMeshProUGUI textBoxTitleTextMesh = default;            // Reference to the Text Box Title Text Mesh Component.
 	[SerializeField] private TextMeshProUGUI textBoxContextTextMesh = default;          // Reference to the Text Box Context Text Mesh Component.
+	[Space]
+	[SerializeField] private IInteractable interactable = default;
+	[SerializeField] private bool interactionPressed = false;
 	#endregion
 
 	#region Properties
 	public TextMeshProUGUI TextBoxTitleTextMesh { get => textBoxTitleTextMesh; set => textBoxTitleTextMesh = value; }
 	public TextMeshProUGUI TextBoxContextTextMesh { get => textBoxContextTextMesh; set => textBoxContextTextMesh = value; }
+	public GameObject TextBoxObject { get => textBoxObject; set => textBoxObject = value; }
+	#endregion
+
+	#region Monobehaviour Callbacks
+	private void Start()
+	{
+		textBoxObject.SetActive(false);
+	}
+
+	private void Update()
+	{
+		interactionPressed = Input.GetAxisRaw("Submit") == 1 ? true : false;
+
+		if(interactionPressed)
+		{
+			textBoxObject.SetActive(true);
+			interactable.Interact(gameObject);
+			interactable.TogglePrompt(false);
+		}
+	}
 	#endregion
 
 	private void OnTriggerEnter(Collider other)
 	{
-		other.GetComponent<IInteractable>()?.Interact(gameObject);
+		if(other.GetComponent<IInteractable>() != null)
+		{
+			interactable = other.GetComponent<IInteractable>();
+			interactable.TogglePrompt(true);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		textBoxObject.SetActive(false);
+		interactable.TogglePrompt(false);
 	}
 }
