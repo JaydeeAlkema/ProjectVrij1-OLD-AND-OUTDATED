3a62b3292fd2bea9c332ba85317337546cb82b12
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-06 21:01:09 +0200

    Add Variables and Properties
    - Add Title Variable.
    - Add Context Variable.
    
    - Add Functionality to change text box on interaction

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour, IInteractable
 {
+	#region Variables
+	[SerializeField] private string title = "";         // Title of the Object / NPC (a.k.a. A Name)
+	[SerializeField] private string context = "";       // The context for this Object / NPC (a.k.a. A Description)
+	#endregion
+
+	#region Properties
+	public string Title { get => title; set => title = value; }
+	public string Context { get => context; set => context = value; }
+	#endregion
+
 	public void Interact(GameObject Interactor)
 	{
+		Interactor.GetComponent<PlayerInteraction>().TextBoxTitleTextMesh.text = title;
+		Interactor.GetComponent<PlayerInteraction>().TextBoxContextTextMesh.text = context;
+
 		Debug.Log(Interactor.name + " Interacted with " + gameObject.name);
 	}
 }
